5117a749bc6aa0995590b42cd787f86e14adb340
Alex Cevallos <Alex.Cevallos@alaskaair.com>
2017-08-09 10:39:24 -0700

    Added Data Annotations to Customer's name

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vidly.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+        [Required] //Makes column Name no longer nullable
+        [StringLength(255)]
         public string Name { get; set; }
         public bool IsSuscribedToNewsLetter { get; set; }
         public MembershipType MembershipType { get; set; }
